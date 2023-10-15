bb813872bb385a03a78bf042be8847da79152c51
shahdibrahiim <Shahd2103789@miuegypt.edu.eg>
2023-10-15 23:19:39 +0300

    adminfront

---
+++ front/Dashboard.js
@@ -0,0 +1,9 @@
+
+
+const ThemeToggler=document.querySelector(".theme-toggler")
+
+ThemeToggler.addEventListener('click',() =>{
+    document.body.classList.toggle('dark-theme-variables');
+    ThemeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    ThemeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
