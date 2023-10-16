62a3b32d9d1e741b426366ca047f518ecf1b8c34
malakhelal <malak2102056@miuegypt.edu.eg>
2023-10-16 16:00:23 +0300

    add product html

---
+++ admin_addproduct.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin add-Product</title>
+    <link rel="stylesheet" href="admin_addproduct.css">
+    <link rel="stylesheet" href="form.css">
+    <link rel="stylesheet" href="Dashboard.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" />
+</head>
+
+<body>
+<div class="container">
+<aside>
+<div class="top">
+                <div class="logo">
+                   <img src="logo.jpeg">
+                    <!-- <h2><span class="danger">Jamila</span></h2>  -->
+                </div>
+                <div class="close" id="close-button">
+                    <span class="material-icons-sharp">close</span>
+                </div>
+            </div>
+            <div class="sidebar">
+               <a href="#" class="active">
+                <span class="material-icons-sharp">grid_view</span>
+                <h3>Dashboard</h3>
+               </a> 
+               <a href="#">
+                <span class="material-icons-sharp">person</span>
+                <h3>Users Accounts</h3>
+               </a> 
+               <a href="#">
+                <span class="material-icons-sharp">person</span>
+                <h3>Admin Accounts</h3>
+               </a> 
+               <a href="admin_addproduct.php">
+                <span class="material-icons-sharp">receipt_long</span>
+                <h3>Add product</h3>
+               </a> 
+               <a href="#">
+                <span class="material-icons-sharp">insights</span>
+                <h3>Edit Product</h3>
+               </a> 
+               <a href="#">
+                <span class="material-icons-sharp">mail_outline</span>
+                <h3>Delete product</h3>
+                <span class="message-count">26</span>
+               </a> 
+               
+               <a href="#">
+                <span class="material-icons-sharp">report_gmailerrorred</span>
+                <h3>Orders</h3>
+               </a> 
+               <a href="#">
+                <span class="material-icons-sharp">logout</span>
+                <h3>Log out</h3>
+               </a> 
+               
+            </div>
+           
+        </aside>
+       
+        <!------------- End Of Sidebar ------------->
