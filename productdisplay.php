ce7e3ab76125817d84a8e108206b9a1e79b7da83
malakhelal <malak2102056@miuegypt.edu.eg>
2023-10-16 18:28:56 +0300

    view produt

--- productdisplay.php
+++ productdisplay.php
@@ -1,3 +1,46 @@
 <?php
 include "makeup.html";
+@include 'config.php';
+$select=mysqli_query($conn,"SELECT * FROM productss");
+?>
+<?php
+
+
+while($row = mysqli_fetch_assoc($select)){
+    if($row['category']=='eye'){
+?>
+
+<div class="box-container">
+
+<div class="box">
+    <div class="container">
+        <div class="img">
+        <img src="uploaded_images/<?php echo $row['image'];?>" height="100" alt="">
+
+
+        </div>
+    </div>
+
+
+    <div class="content">
+        <h3><?php echo $row['name'];?></h3>
+        <h3><?php echo $row['description'];?></h3>
+        <div style="margin:10px ;padding:10px" class="main">
+            <i class="fa fa-star checked" id="one"></i>
+            <i class="fa fa-star unchecked" id="two"></i>
+            <i class="fa fa-star unchecked" id="three"></i>
+            <i class="fa fa-star unchecked" id="four"></i>
+            <i class="fa fa-star unchecked" id="five"></i>
+        </div>
+    
+        <div class="btn1-group">
+            <button><a href="#"> A d d t o B A G <?php echo $row['price']; ?>$</a></button>
+        </div>
+    </div>
+
+
+</div>
+<?php
+
+}}
 ?>
