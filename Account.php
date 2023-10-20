bf63cb92159ac981e19873e772077ae1a5599e59
shahdibrahiim <Shahd2103789@miuegypt.edu.eg>
2023-10-20 14:30:52 +0300

    signin

--- Account.php
+++ Account.php
@@ -19,5 +19,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $row = mysqli_fetch_assoc($result);
         $hashedPassword = $row['Password'];
 
+        // Verify if the entered password matches the hashed password
+        if (password_verify($Password, $hashedPassword)) {
+            $_SESSION["ID"] = $row["ID"];
+            $_SESSION["FName"] = $row["FirstName"];
+            $_SESSION["LName"] = $row["LastName"];
+            $_SESSION["Email"] = $row["Email"];
+            $_SESSION["UserType"] = $row["UserType"];
+
+            if ($_SESSION["UserType"] == "admin") {
+                header("Location: admindashboard.php");
+            } else {
+                header("Location: homee.php");
+            }
+            exit();
+        } else {
+            echo "Invalid password.";
+        }
+    } else {
+        echo "Email does not exist.";
+    }
+}
+
+// Add the code below to prevent non-admin users from accessing the admindashboard.php page
+if (isset($_SESSION["UserType"]) && $_SESSION["UserType"] !== "admin") {
+    echo "You are not authorized to access this page.";
+    exit();
+}
+
+
 
 ?>
