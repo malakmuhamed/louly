6d7794c9c2901a9c1843a89d1e38318aa44fd9a8
shahdibrahiim <Shahd2103789@miuegypt.edu.eg>
2023-10-20 14:34:13 +0300

    d

--- Delete.php
+++ Delete.php
@@ -2,5 +2,10 @@
 session_start();
 include_once "includes/dbh.inc.php";
 // Check if the user is an administrator
+if (!isset($_SESSION["UserType"]) || $_SESSION["UserType"] !== "admin") {
+    echo "You are not authorized to access this page.";
+    exit();
+}
 
+include "Admin Dashboard.html";
 ?>
