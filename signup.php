d3bda3ae2fbf444cc8c55afe46de863d13835ad4
shahd soliman <shahd2101760@miuegypt.edu.eg>
2023-10-20 15:00:51 +0300

    c

--- signup.php
+++ signup.php
@@ -38,3 +38,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Check if password and confirm password match
+    if ($Password !== $ConfirmPassword) {
+        $errors[] = "Password and confirm password do not match.";
+    }
+
+    // Check minimum password length
+    if (strlen($Password) < 8) {
+        $errors[] = "Password must be at least 8 characters long.";
+    }
+
+    // Check if email is already registered
+    $sql = "SELECT COUNT(*) FROM users WHERE Email = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $Email);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $emailCount);
+    mysqli_stmt_fetch($stmt);
+    mysqli_stmt_close($stmt);
+
+    if ($emailCount > 0) {
+        $errors[] = "Email is already registered.";
